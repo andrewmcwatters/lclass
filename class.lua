c6e02627508cc0392a3b8bc7516f1408bacbe0d2
Andrew McWatters <mcwattersandrew@gmail.com>
2016-05-16 11:09:53 -0700

    Added real-time class redefintions (#3)
    * Added real-time class redefintions
    
    Added _G.getbaseclass
    
    * Patched assignment of __index
    
    * Removed unnecessary __base check
    
    * Removed dependency on Grid SDK Lua 5.1.5 base library extensions

--- class.lua
+++ class.lua
@@ -44,7 +44,15 @@ local eventnames = {
 -------------------------------------------------------------------------------
 local function metamethod( class, eventname )
 	return function( ... )
-		local event = class.__base[ eventname ]
+		local event = nil
+		local base = getbaseclass( class )
+		while ( base ~= nil ) do
+			if ( base[ eventname ] ) then
+				event = base[ eventname ]
+				break
+			end
+			base = getbaseclass( base )
+		end
 		local type = type( event )
 		if ( type ~= "function" ) then
 			error( "attempt to call metamethod '" .. eventname .. "' " ..
@@ -60,18 +68,47 @@ local function metamethod( class, eventname )
 end
 
 -------------------------------------------------------------------------------
+-- classes
+-- Purpose: Store classes for real-time class redefining
+-------------------------------------------------------------------------------
+local classes = {}
+
+-------------------------------------------------------------------------------
+-- getbaseclass()
+-- Purpose: Get a base class
+-- Input: class - Class metatable
+-- Output: class
+-------------------------------------------------------------------------------
+local function getbaseclass( class )
+	local name = class.__base
+	return classes[ name ]
+end
+
+_G.getbaseclass = getbaseclass
+
+-------------------------------------------------------------------------------
 -- class()
 -- Purpose: Creates a new class
 -- Input: name - Name of new class
 -------------------------------------------------------------------------------
 function class( name )
-	local metatable = {}
+	local metatable = classes[ name ]
+	if ( metatable ) then
+		for k in pairs( metatable ) do
+			metatable[ k ] = nil
+		end
+	else
+		metatable = {}
+		classes[ name ] = metatable
+	end
+
 	metatable.__index = metatable
 	metatable.__type = name
 	-- Create a shortcut to name()
 	setmetatable( metatable, {
 		__call = function( _, ... )
 			-- Create a new instance of this object
+			local metatable = classes[ name ]
 			local object = new( metatable )
 			-- Call its constructor (function name:name( ... ) ... end) if it
 			-- exists
@@ -94,19 +131,21 @@ function class( name )
 	return function( base )
 		-- Set our base class to the class definition in the function
 		-- environment we called from
+		local metatable = classes[ name ]
 		if ( type( base ) == "string" ) then
-			metatable.__base = getfenv( 2 )[ base ]
+			metatable.__base = getfenv( 2 )[ base ].__type
 		else
 			-- Otherwise set the base class directly
-			metatable.__base = base
+			metatable.__base = base.__type
 		end
 		-- Overwrite our existing __index value with a metamethod which checks
 		-- our members, metatable, and base class, in that order, a la behavior
 		-- via the Lua 5.1 manual's illustrative code for indexing access
 		metatable.__index = function( table, key )
+			local metatable = classes[ name ]
 			local v = rawget( metatable, key )
 			if ( v ~= nil ) then return v end
-			local h = rawget( metatable.__base, "__index" )
+			local h = rawget( getbaseclass( metatable ), "__index" )
 			if ( h == nil ) then return nil end
 			if ( type( h ) == "function" ) then
 				return h( table, key )
